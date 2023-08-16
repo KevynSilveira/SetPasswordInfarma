188b1dac613539fd388562a32d17215f5e5c8b8b
Kevyn <kevynsilveira0610@gmail.com>
2023-08-16 09:21:26 -0300

    Feitos a integração com banco

--- main.py
+++ main.py
@@ -1,11 +1,58 @@
-
 import customtkinter as ctk
 import tkinter as tk
+import pyodbc
 from tkinter import messagebox
 from customtkinter import set_appearance_mode
 
+def access_db(): # Acessa o banco de dados
+    global conn, cursor  # Utiliza as variáveis globais
 
+    try:
+        # Credenciais do banco de dados
+        server = "192.168.64.5"
+        database = "DMD"
+        username = ""
+        password = ""
 
+        # Monta os dados para enviar para o banco de dados (credenciais)
+        conn_string = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+        conn = pyodbc.connect(conn_string)
+        cursor = conn.cursor()
+    except:
+        messagebox.showerror("ATENÇÃO", "Erro ao conectar no banco")
+    return conn, cursor
+
+def close_db(): # Fecha a conexão com o banco de dados.
+    global conn, cursor  # Utiliza as variáveis globais
+    if cursor is not None:
+        cursor.close()
+    if conn is not None:
+        conn.close()
+
+def update_db(): # Faz o update
+    global cursor # Pega o cursor global
+
+    try:
+        query_update =""
+
+        cursor.execute(query_update) # Executa a query
+        conn.commit() # Confirma o update
+
+        rows_affected = cursor.rowcount # Verifica quantas linhas foram afetadas
+        messagebox.showinfo("ATENÇÃO", f"Foram afetadas {rows_affected} linhas!")
+        print(f"Foram afetadas {rows_affected} linhas!")
+        return rows_affected # Retorna o valor da linha para armazenar em uma variavel
+
+    except pyodbc.Error as e:
+        messagebox.showerror("ATENÇÃO", f"Erro ao executar a consulta no banco de dados: {e}")
+
+def execute():
+    try:
+        access_db()
+        update_db()
+        close_db()
+    except:
+        messagebox.showerror("ATENÇÃO", "Preencha todos os campos!")
 
 frame = ctk.CTk()
 frame.geometry("250x200")
